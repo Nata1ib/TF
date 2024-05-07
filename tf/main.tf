880f63f84020b18338a25c0b7a139e982eabfbc7
Nata1ib <113211727+Nata1ib@users.noreply.github.com>
2024-05-07 23:55:20 +0300

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -1,65 +1,76 @@
+# Define required providers
 terraform {
-  required_version = ">= 0.14.0"
+required_version = ">= 0.14.0"
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.39.0"
+      version = "~> 1.53.0"
     }
   }
 }
 
+# Configure the Vault Provider by $VAULT_ADDR and $VAULT_TOKEN
+provider "vault" {}
+
+data "vault_kv_secret_v2" "auth_secret" {
+  mount = "kv"
+  name  = "openstack-auth"
+}
+
+# Configure the OpenStack Provider
 provider "openstack" {
-  auth_url = "https://cloud.crplab.ru:5000"
-  tenant_id = "a02aed7892fa45d0bc2bef3b8a08a6e9"
-  tenant_name = "students"
-  user_domain_name = "Default"
-  user_name = "master2022"
-  password = var.passwd
-  region = "RegionOne"
+  user_name   = data.vault_kv_secret_v2.auth_secret.data["username"]
+  tenant_id   = data.vault_kv_secret_v2.auth_secret.data["project_id"]
+  password    = data.vault_kv_secret_v2.auth_secret.data["password"]
+  auth_url    = data.vault_kv_secret_v2.auth_secret.data["auth_url"]
 }
 
-resource "openstack_networking_secgroup_v2" "sec_group" {
-  name        = "w_bot_sec_group"
+# Define security group
+resource "openstack_networking_secgroup_v2" "glezova_tg_secgroup" {
+  name        = "boytsova_tg_secgroup"
+  description = "Security group for ssh and http/https"
 }
 
-resource "openstack_networking_secgroup_rule_v2" "sec_group_ssh_rule" {
+# Security group rule for ssh
+resource "openstack_networking_secgroup_rule_v2" "ssh_rule" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
   port_range_min    = 22
   port_range_max    = 22
   remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.sg.id
+  security_group_id = openstack_networking_secgroup_v2.boytsova_tg_secgroup.id
 }
 
-resource "openstack_networking_secgroup_rule_v2" "sec_group_http_rule" {
+# Security group rule for http
+resource "openstack_networking_secgroup_rule_v2" "http_rule" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
   port_range_min    = 80
   port_range_max    = 80
   remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.sec_group.id
+  security_group_id = openstack_networking_secgroup_v2.boytsova_tg_secgroup.id
 }
 
 # Security group rule for https
-resource "openstack_networking_secgroup_rule_v2" "sec_group_https_rule" {
+resource "openstack_networking_secgroup_rule_v2" "https_rule" {
   direction         = "ingress"
   ethertype         = "IPv4"
   protocol          = "tcp"
   port_range_min    = 443
   port_range_max    = 443
   remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.sec_group.id
+  security_group_id = openstack_networking_secgroup_v2.boytsova_tg_secgroup.id
 }
 
-
-resource "openstack_compute_instance_v2" "w_bot" {
-  name        = "w_bot"
-  image_name  = var.image_name
-  flavor_name = var.flavor_name
-  key_pair = var.key_pair
-  security_groups = [openstack_networking_secgroup_v2.sg.name]
+# Configure an instance
+resource "openstack_compute_instance_v2" "boytsova_bot" {
+  name              = "glezova_bot_tf"
+  image_name        = var.image_name
+  flavor_name       = var.flavor_name
+  key_pair          = var.key_pair
+  security_groups   = [openstack_networking_secgroup_v2.boytsova_tg_secgroup.name]
 
   network {
     name = var.network_name
