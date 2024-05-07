9aaaccab36b3b1fe500b3a5fecc49ee71118ab8f
Nata1ib <113211727+Nata1ib@users.noreply.github.com>
2024-05-07 21:47:59 +0300

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -8,22 +8,18 @@ terraform {
   }
 }
 
-provider "vault" {
-}
-
-data "vault_generic_secret" "stack_orchestration" {
-  path = "secret/stack_orchestration"
-}
-
 provider "openstack" {
-  auth_url  = data.vault_generic_secret.stack_orchestration.data["auth_url"]  
-  password  = data.vault_generic_secret.stack_orchestration.data["password"]
-  tenant_id = data.vault_generic_secret.stack_orchestration.data["project_id"]
-  user_name = data.vault_generic_secret.stack_orchestration.data["username"]
+  auth_url = "https://cloud.crplab.ru:5000"
+  tenant_id = "a02aed7892fa45d0bc2bef3b8a08a6e9"
+  tenant_name = "students"
+  user_domain_name = "Default"
+  user_name = "master2022"
+  password = var.passwd
+  region = "RegionOne"
 }
 
 resource "openstack_networking_secgroup_v2" "sec_group" {
-  name      = "w_bot_sec_group"
+  name        = "w_bot_sec_group"
 }
 
 resource "openstack_networking_secgroup_rule_v2" "sec_group_ssh_rule" {
@@ -33,7 +29,7 @@ resource "openstack_networking_secgroup_rule_v2" "sec_group_ssh_rule" {
   port_range_min    = 22
   port_range_max    = 22
   remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.sec_group.id
+  security_group_id = openstack_networking_secgroup_v2.sg.id
 }
 
 resource "openstack_networking_secgroup_rule_v2" "sec_group_http_rule" {
@@ -57,14 +53,15 @@ resource "openstack_networking_secgroup_rule_v2" "sec_group_https_rule" {
   security_group_id = openstack_networking_secgroup_v2.sec_group.id
 }
 
+
 resource "openstack_compute_instance_v2" "w_bot" {
-  name              = "w_bot"
-  image_name        = var.image_name
-  flavor_name       = var.flavor_name
-  key_pair          = var.key_pair
-  security_groups   = [openstack_networking_secgroup_v2.sec_group.name]
+  name        = "task_manager_tg"
+  image_name  = var.image_name
+  flavor_name = var.flavor_name
+  key_pair = var.key_pair
+  security_groups = [openstack_networking_secgroup_v2.sg.name]
 
   network {
-    name            = var.network_name
+    name = var.network_name
   }
 }
